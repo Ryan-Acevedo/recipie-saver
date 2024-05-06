88b38c213b7d6815c5ab4c071990585d2de3770e
Zeb Thomas <zeb.thomas211@gmail.com>
2024-05-05 20:23:19 -0500

    Update Unauthorized.jsx

--- client/src/views/Unauthorized.jsx
+++ client/src/views/Unauthorized.jsx
@@ -7,7 +7,7 @@ const Unauthorized = (props) => {
         <div className='flex flex-col justify-center items-center'>
             <header className='w-full bg-gray-950 flex flex-row justify-between items-center p-5' >
                 <div>
-                    <h1 className='text-gray-100 decoration-solid text-6xl italic' >Unathorized</h1>
+                    <h1 className='text-gray-100 decoration-solid text-6xl italic' >Unauthorized</h1>
                 </div>
                 <div>
                     {
@@ -19,7 +19,7 @@ const Unauthorized = (props) => {
             </header>
             <div className='flex flex-col w-full justify-start items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%' style={{ height: '100vh' }}>
                 <div className='m-8 bg-myColor-50 p-5 shadow-2xl shadow-gray-950' >
-                    <h2 className='text-red-600 text-4xl mb-4'>Unathorized: Access is denied due to invalid credentials.</h2>
+                    <h2 className='text-red-600 text-4xl mb-4'>Unauthorized: Access is denied due to invalid credentials.</h2>
                     <h2 className='text-myColor-900 text-4xl mb-4'>You do not have permission to view this page. Please login or register to continue.</h2>
                 </div>
             </div>
